9ccf38a0a581e2475d74a265ae913c3f65142cd8
AndreiIlin <andrei362065@gmail.com>
2022-11-15 15:53:05 +0300

    init commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@features': path.resolve(__dirname, './src/features'),
+      '@infrastructure': path.resolve(__dirname, './src/infrastructure'),
+      '@screens': path.resolve(__dirname, './src/screens'),
+      '@ui-library': path.resolve(__dirname, './src/ui-library'),
+      // '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
+})
